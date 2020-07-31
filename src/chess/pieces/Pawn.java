8be6a216e0c9efc0f453a9a8155f34854fcdd3a3
Rhuan Menezes <rhuann@gmail.com>
2020-07-31 10:33:00 -0400

    Implementando a peça Bispo

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -15,7 +15,7 @@ public class Pawn extends ChessPiece {
 	public String toString() {
 		return "P";
 	}
-	
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
